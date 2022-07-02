7ea487629f223fc3f09fbd7dd055f7541852ec56
azamatdoronov <aza3el@inbox.ru>
2022-07-02 14:41:43 +0600

    Сделал главную страницу index.html, а также отображение активных записей.  Создал base.hmtl, style.css

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from webapp.models import Guestbook, STATUS_CHOICES
+
+
+def index_view(request):
+    guestbooks = Guestbook.objects.order_by("-update_nt")
+    context = {"guestbooks": guestbooks}
+    return render(request, "index.html", context)
+
+
+
+
+
+
