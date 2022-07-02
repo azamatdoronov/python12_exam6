ada7bb2d687087c39de14df77abc69129af0f0b0
azamatdoronov <aza3el@inbox.ru>
2022-07-02 18:04:37 +0600

    Сделал изменения связанные с бутстрапом (добавил стилей)

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,7 +3,7 @@ from django.forms import widgets
 
 
 class GuestbookForm(forms.Form):
-    author = forms.CharField(max_length=20, required=True, label='Автор')
-    email = forms.EmailField(max_length=20, empty_value=True, required=True, label='Email')
+    author = forms.CharField(max_length=20, required=True, label='Автор:')
+    email = forms.EmailField(max_length=20, empty_value=True, required=True, label='Email:')
     note = forms.CharField(max_length=2000, required=True, widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}),
-                           label="Текст")
+                           label="Текст:")
